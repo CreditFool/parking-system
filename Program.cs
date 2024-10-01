2bd37f3873b0367e1f362c776d7972135cab094d
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-10-01 17:18:08 +0700

    project init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Vehicle vehicle;
+
+String command;
+
+do
+{
+    Console.Write("$ ");
+    command = Console.ReadLine();
+    Console.WriteLine(CommandValidatorUtil.Validate(command));
+} while (command != "exit");
